247bf92a26546034fda864700da1cba46688d8ce
Afonso Mukai <28659574+amues@users.noreply.github.com>
2018-08-14 16:28:56 +0200

    Add contents to custom-theme.js

--- custom-theme.js
+++ custom-theme.js
@@ -1,1 +1,16 @@
-// This is a test file.
+// Solution from https://github.com/jan-molak/jenkins-build-monitor-plugin/issues/270#issuecomment-372725637.
+
+// Clean up names
+function cleanBuildMonitorNames(){
+  document.querySelectorAll('#widgets h2 a').forEach(function(a) {
+    // remove org name
+    a.innerHTML = a.innerHTML.replace('European Spallation Source ', 'THIS IS A TEST ');
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  if(document.body.classList.contains('build-monitor')) {
+    setTimeout(cleanBuildMonitorNames, 100)
+    setTimeout(cleanBuildMonitorNames, 500)
+  }
+});
