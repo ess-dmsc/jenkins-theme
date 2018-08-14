64455c79bdd4a0b2b9b719fe6d51e5a54f2888c7
Afonso Mukai <28659574+amues@users.noreply.github.com>
2018-08-14 17:07:53 +0200

    Remove organisation name

--- theme.js
+++ theme.js
@@ -4,7 +4,7 @@
 function cleanBuildMonitorNames(){
   document.querySelectorAll('#widgets h2 a').forEach(function(a) {
     // remove org name
-    a.innerHTML = a.innerHTML.replace('European Spallation Source - Data Management and Software Centre', 'ess-dmsc');
+    a.innerHTML = a.innerHTML.replace('European Spallation Source - Data Management and Software Centre » ', '');
   })
 }
 
